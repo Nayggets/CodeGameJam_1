a61ac23b94ceeff952a2b7ef430c7a6eee555fcd
Luckyponk <95107804+Luckyponk@users.noreply.github.com>
2022-01-18 19:45:22 +0100

    Add files via upload

--- Scripts/PlayerCombatController.cs
+++ Scripts/PlayerCombatController.cs
@@ -11,15 +11,61 @@ public class PlayerCombatController : MonoBehaviour
     public LayerMask whatIsEnemies;
     public float attackRange;
     public float damage;
+    private Rigidbody2D rb;
+
+    [Header("Life")]
+    [SerializeField] private int maxHealth;
+    [SerializeField] private int actualHealth;
+    private bool canTakeDamage = true;
+
+    void OnCollisionEnter2D(Collision2D col){
+        if(col.gameObject.CompareTag("Enemy")){
+            TakeDamage(1, col.transform.position.x);
+        }
+    }
+
+    public void TakeDamage(int damage, float position){
+        if(canTakeDamage){
+            canTakeDamage = false;
+            actualHealth -= damage;
+            StartCoroutine(timeTakeDamage());
+            StartCoroutine(Knockback(0.02f, 350f, transform.position, position));
+        }
+    }
+
+    public IEnumerator Knockback(float knockDur, float knockBackPwr, Vector3 knockbackDir, float position){
+        float timer = 0;
+
+        while (knockDur > timer){
+            timer +=Time.deltaTime;
+            if(position > transform.position.x){
+                rb.AddForce(new Vector3(knockbackDir.x * -100, knockbackDir.y * knockBackPwr, transform.position.z));
+            }
+            else{
+                rb.AddForce(new Vector3(knockbackDir.x * 100, knockbackDir.y * knockBackPwr, transform.position.z));
+            }
+        }
+
+        yield return 0;
+    }
+
+    private IEnumerator timeTakeDamage(){
+        yield return new WaitForSeconds(0.5f);
+        canTakeDamage = true;
+    }
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        actualHealth = maxHealth;
+    }
 
     // Update is called once per frame
     void Update()
     {
         if(timeBtwAttack <= 0)
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButton(0) && ChangeWeapon.currentWeaponNumber == 0 && !PlayerController.isSwimming)
             {
-                print("clique");
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies) ;
                 for (int i = 0; i < enemiesToDamage.Length; i++)
                 {
